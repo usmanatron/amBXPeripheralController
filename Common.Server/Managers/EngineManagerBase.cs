f11bdc2a27be29a60281f0c3b755813a1b622dc7
Usman Iqbal <usman.iqbal@softwire.com>
2013-12-22 17:31:30 +0000

    Commonise (and clean up) large chunks of the engine manager

--- Common.Server/Managers/EngineManagerBase.cs
+++ Common.Server/Managers/EngineManagerBase.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Common.Entities;
+using Common.Integration;
+using System;
 using amBXLib;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Common.Server.Managers
 {
@@ -9,25 +13,112 @@ namespace Common.Server.Managers
     protected EngineManagerBase()
     {
       mEngine = new amBX(1, 0, "amBXNotification", "1.0");
+      mLights = new Dictionary<CompassDirection, amBXLight>();
+      mFans = new Dictionary<CompassDirection, amBXFan>();
+      mRumbles = new Dictionary<CompassDirection, amBXRumble>();
       InitialiseEngine(mEngine);
     }
 
     private void InitialiseEngine(amBX xiEngine)
     {
-      NorthLight     = xiEngine.CreateLight(CompassDirection.North,     RelativeHeight.AnyHeight);
-      NorthEastLight = xiEngine.CreateLight(CompassDirection.NorthEast, RelativeHeight.AnyHeight);
-      EastLight      = xiEngine.CreateLight(CompassDirection.East,      RelativeHeight.AnyHeight);
-      SouthEastLight = xiEngine.CreateLight(CompassDirection.SouthEast, RelativeHeight.AnyHeight);
-      SouthLight     = xiEngine.CreateLight(CompassDirection.South,     RelativeHeight.AnyHeight);
-      SouthWestLight = xiEngine.CreateLight(CompassDirection.SouthWest, RelativeHeight.AnyHeight);
-      WestLight      = xiEngine.CreateLight(CompassDirection.West,      RelativeHeight.AnyHeight);
-      NorthWestLight = xiEngine.CreateLight(CompassDirection.NorthWest, RelativeHeight.AnyHeight);
+      CreateLight(CompassDirection.North);
+      CreateLight(CompassDirection.NorthEast);
+      CreateLight(CompassDirection.East);
+      CreateLight(CompassDirection.SouthEast);
+      CreateLight(CompassDirection.South);
+      CreateLight(CompassDirection.SouthWest);
+      CreateLight(CompassDirection.West);
+      CreateLight(CompassDirection.NorthWest);
 
-      EastFan = xiEngine.CreateFan(CompassDirection.East, RelativeHeight.AnyHeight);
-      WestFan = xiEngine.CreateFan(CompassDirection.West, RelativeHeight.AnyHeight);
+      CreateFan(CompassDirection.East);
+      CreateFan(CompassDirection.West);
 
-      Rumble = xiEngine.CreateRumble(CompassDirection.Everywhere, RelativeHeight.AnyHeight);
+      CreateRumble(CompassDirection.Everywhere);
     }
+
+    private void CreateLight(CompassDirection xiDirection)
+    {
+      var lLight = mEngine.CreateLight(xiDirection, RelativeHeight.AnyHeight);
+      mLights.Add(xiDirection, lLight);
+    }
+
+    private void CreateFan(CompassDirection xiDirection)
+    {
+      var lFan = mEngine.CreateFan(xiDirection, RelativeHeight.AnyHeight);
+      mFans.Add(xiDirection, lFan);
+    }
+
+    private void CreateRumble(CompassDirection xiDirection)
+    {
+      var lRumble = mEngine.CreateRumble(xiDirection, RelativeHeight.AnyHeight);
+      mRumbles.Add(xiDirection, lRumble);
+    }
+
+    #region Updating
+
+    protected void UpdateLight(CompassDirection xiDirection, Light xiInputLight, int xiFadeTime)
+    {
+      UpdateLightInternal(mLights[xiDirection], xiInputLight, xiFadeTime);
+    }
+
+    private void UpdateLightInternal(amBXLight xiLight, Light xiInputLight, int xiFadeTime)
+    {
+      if (xiInputLight == null)
+      {
+        // No change - don't touch!
+        return;
+      }
+      xiLight.Color = new amBXColor { Red = xiInputLight.Red, Green = xiInputLight.Green, Blue = xiInputLight.Blue };
+      xiLight.FadeTime = xiFadeTime;
+    }
+
+    protected void UpdateFan(CompassDirection xiDirection, Fan xiInputLight)
+    {
+      UpdateFanInternal(mFans[xiDirection], xiInputLight);
+    }
+
+    private void UpdateFanInternal(amBXFan xiFan, Fan xiInputFan)
+    {
+      if (xiInputFan == null)
+      {
+        return;
+      }
+      xiFan.Intensity = xiInputFan.Intensity;
+    }
+
+    protected void UpdateRumble(CompassDirection xiDirection, RumbleComponent xiInputRumble)
+    {
+      UpdateRumbleInternal(mRumbles[xiDirection], xiInputRumble);
+    }
+
+
+    protected void UpdateRumbleInternal(amBXRumble xiRumble, RumbleComponent xiInputRumble)
+    {
+      if (xiInputRumble == null)
+      {
+        return;
+      }
+
+      RumbleType lRumbleType;
+
+      try
+      {
+        lRumbleType = RumbleTypeConverter.GetRumbleType(xiInputRumble.RumbleType);
+      }
+      catch (InvalidRumbleException)
+      {
+        return;
+      }
+
+      xiRumble.RumbleSetting = new amBXRumbleSetting
+      {
+        Intensity = xiInputRumble.Intensity,
+        Speed = xiInputRumble.Speed,
+        Type = lRumbleType
+      };
+    }
+
+    #endregion
 
     public void Dispose()
     {
@@ -36,30 +127,8 @@ namespace Common.Server.Managers
 
     private readonly amBX mEngine;
 
-    #region amBXLib Members
-
-    #region Lights
-
-    protected amBXLight NorthLight;
-    protected amBXLight NorthEastLight;
-    protected amBXLight EastLight;
-    protected amBXLight SouthEastLight;
-    protected amBXLight SouthLight;
-    protected amBXLight SouthWestLight;
-    protected amBXLight WestLight;
-    protected amBXLight NorthWestLight;
-
-    #endregion
-
-    #region Fans
-
-    protected amBXFan EastFan;
-    protected amBXFan WestFan;
-
-    #endregion
-
-    protected amBXRumble Rumble;
-
-    #endregion
+    private Dictionary<CompassDirection, amBXLight> mLights;
+    private Dictionary<CompassDirection, amBXFan> mFans;
+    private Dictionary<CompassDirection, amBXRumble> mRumbles;
   }
 }
--- Server/EngineManager.cs
+++ Server/EngineManager.cs
@@ -8,74 +8,27 @@ namespace Server
   // Manages the amBXEngine interface - deals with adding and setting stuff etc.
   class EngineManager : EngineManagerBase
   {
-
     internal void UpdateLights(LightComponent xiLights)
     {
-      UpdateLight(NorthLight, xiLights.North, xiLights.FadeTime);
-      UpdateLight(NorthEastLight, xiLights.NorthEast, xiLights.FadeTime);
-      UpdateLight(EastLight, xiLights.East, xiLights.FadeTime);
-      UpdateLight(SouthEastLight, xiLights.SouthEast, xiLights.FadeTime);
-      UpdateLight(SouthLight, xiLights.South, xiLights.FadeTime);
-      UpdateLight(SouthWestLight, xiLights.SouthWest, xiLights.FadeTime);
-      UpdateLight(WestLight, xiLights.West, xiLights.FadeTime);
-      UpdateLight(NorthWestLight, xiLights.NorthWest, xiLights.FadeTime);
-    }
-
-    private void UpdateLight(amBXLight xiLight, Light xiInputLight, int xiFadeTime)
-    {
-      if (xiInputLight == null)
-      {
-        // No change - don't touch!
-        return;
-      }
-      xiLight.Color = new amBXColor { Red = xiInputLight.Red, Green = xiInputLight.Green, Blue = xiInputLight.Blue };
-      xiLight.FadeTime = xiFadeTime;
+      UpdateLight(CompassDirection.North, xiLights.North, xiLights.FadeTime);
+      UpdateLight(CompassDirection.NorthEast, xiLights.NorthEast, xiLights.FadeTime);
+      UpdateLight(CompassDirection.East, xiLights.East, xiLights.FadeTime);
+      UpdateLight(CompassDirection.SouthEast, xiLights.SouthEast, xiLights.FadeTime);
+      UpdateLight(CompassDirection.South, xiLights.South, xiLights.FadeTime);
+      UpdateLight(CompassDirection.SouthWest, xiLights.SouthWest, xiLights.FadeTime);
+      UpdateLight(CompassDirection.West, xiLights.West, xiLights.FadeTime);
+      UpdateLight(CompassDirection.NorthWest, xiLights.NorthWest, xiLights.FadeTime);
     }
 
     internal void UpdateFans(FanComponent xiFans)
     {
-      UpdateFan(EastFan, xiFans.East);
-      UpdateFan(WestFan, xiFans.West);
-    }
-
-    private void UpdateFan(amBXFan xiFan, Fan xiInputFan)
-    {
-      if (xiInputFan == null)
-      {
-        return;
-      }
-      xiFan.Intensity = xiInputFan.Intensity;
+      UpdateFan(CompassDirection.East, xiFans.East);
+      UpdateFan(CompassDirection.West, xiFans.West);
     }
 
     internal void UpdateRumbles(RumbleComponent xiInputRumble)
     {
-      UpdateRumble(Rumble, xiInputRumble);
-    }
-
-    private void UpdateRumble(amBXRumble xiRumble, RumbleComponent xiInputRumble)
-    {
-      if (xiInputRumble == null)
-      {
-        return;
-      }
-
-      RumbleType lRumbleType;
-
-      try
-      {
-        lRumbleType = RumbleTypeConverter.GetRumbleType(xiInputRumble.RumbleType);
-      }
-      catch (InvalidRumbleException)
-      {
-        return;
-      }
-
-      xiRumble.RumbleSetting = new amBXRumbleSetting
-                               {
-                                 Intensity = xiInputRumble.Intensity,
-                                 Speed = xiInputRumble.Speed,
-                                 Type = lRumbleType
-                               };
+      UpdateRumble(CompassDirection.Everywhere, xiInputRumble);
     }
   }
 }
