33b15c29da013b5463c494a3d844f1e848265285
Usman Iqbal <umi@softwire.com>
2014-02-05 17:28:24 +0000

    Add empty Client.Tests project

---
+++ aPC.Client.Tests/ArgumentReaderTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using aPC.Client;
+
+namespace aPC.Client.Tests
+{
+  [TestFixture]
+  public class ArgumentReaderTests
+  {
+    [Test]
+    [TestCase()]
+    public void NotHavingTwoArguments_ThrowsException(List<string> xiArguments)
+    {
+      //Assert.Throws<UsageException>(new ArgumentReader());
+    }
+
+
+    /*
+     * Should throw if !=2 arguments
+     * First arg must be /I or /f, otherwise throw
+     */
+  }
+}
