ac81a32a063549c8860b021c2e9cbb508f197507
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-08 17:39:53 +0000

    Tests for the new PhysicalComponent-based methods in LightSectionBuilder

--- aPC.Common.Tests/Builders/LightSectionBuilderTests.cs
+++ aPC.Common.Tests/Builders/LightSectionBuilderTests.cs
@@ -114,6 +114,46 @@ namespace aPC.Common.Tests.Builders
       Assert.AreEqual(mOrange, lSection.NorthWest);
     }
 
+    /// <remarks>
+    /// In order to confirm the light is not added, we show that the Builder is invalid
+    /// It clearly has a fade time, so this must be from the lack of light.
+    /// </remarks>
+    [Test]
+    public void SpecifyingAPhysicalLightInANonPhysicalLocation_DoesNotUpdateLight()
+    {
+      var lSectionBuilder = new LightSectionBuilder()
+        .WithFadeTime(100)
+        .WithLightInDirectionIfPhysical(eDirection.South, mBlue);
+      
+      Assert.Throws<ArgumentException>(() => lSectionBuilder.Build());
+    }
+
+    [Test]
+    public void SpecifyingAPhysicalLightInaNonPhysicalDirection_DoesNotOverwritePreviousData()
+    {
+      var lSection = new LightSectionBuilder()
+        .WithFadeTime(100)
+        .WithLightInDirection(eDirection.South, mGreen)
+        .WithLightInDirectionIfPhysical(eDirection.South, mBlue)
+        .Build();
+
+      Assert.AreEqual(mGreen, lSection.South);
+    }
+
+    [Test]
+    public void SpecifyingAPhysicalLightInAPhysicalDirection_UpdatesLight()
+    {
+      var lSectionBuilder = new LightSectionBuilder()
+        .WithFadeTime(100)
+        .WithLightInDirectionIfPhysical(eDirection.North, mGreen);
+
+      Assert.DoesNotThrow(() => lSectionBuilder.Build());
+
+      var lSection = lSectionBuilder.Build();
+
+      Assert.AreEqual(mGreen, lSection.North);
+    }
+
     private Light mOrange = DefaultLights.Orange;
     private Light mGreen = DefaultLights.Green;
     private Light mBlue = DefaultLights.Blue;
