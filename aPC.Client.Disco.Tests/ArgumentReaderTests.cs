d6f47b142bf8e62b6915536add5c18f95f1ea5b4
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-11 22:17:02 +0000

    Rewrite ArgumentReader tests

--- aPC.Client.Disco.Tests/ArgumentReaderTests.cs
+++ aPC.Client.Disco.Tests/ArgumentReaderTests.cs
@@ -29,22 +29,66 @@ namespace aPC.Client.Disco.Tests
     }
 
     [Test]
-    public void SpecifyingAnyRangeNotBetween0and1_ThrowsException()
+    public void UnexpectedArgument_ThrowsException()
     {
-      var lArguments = new List<string> { "red:0,3" };
-      Assert.Throws<UsageException>(() => new ArgumentReader(lArguments).ParseArguments());
+      var lArguments = new List<string> { "TotallyNonExistantArgument:0,1" };
+      var lArgumentReader = new ArgumentReader(lArguments);
+      Assert.Throws<UsageException>(() => lArgumentReader.ParseArguments());
     }
 
-    //qqUMI more test cases
     [Test]
-    public void SpecifyingACustomRange_CorrectlyAddedToSettings()
+    [TestCaseSource("GetArgumentCases")]
+    public void SpecifyingAnOverridingArgument_CorrectlyAddedToSettings(SettingsTester xiSettingsTest)
     {
-      var lArguments = new List<string> { "red:0.3,0.7" };
-      var lArgumentSettings = new ArgumentReader(lArguments).ParseArguments();
+      var lArgs = new List<string> { xiSettingsTest.Argument };
 
-      Assert.AreEqual(new Range(0.3f, 0.7f), lArgumentSettings.RedColourWidth);
+      var lArgumentSettings = new ArgumentReader(lArgs).ParseArguments();
+
+      Assert.AreEqual(xiSettingsTest.ExpectedValue, xiSettingsTest.RangeSelector(lArgumentSettings));
+    }
+
+    private readonly object[] GetArgumentCases = 
+    {
+      new SettingsTester(new Range(0.3f, 0.7f), "red:0.3,0.7", settings => settings.RedColourWidth),
+      new SettingsTester(new Range(0.21f, 0.89f), "green:0.21,0.89", settings => settings.GreenColourWidth),
+      new SettingsTester(new Range(0.1f, 0.55f), "blue:0.1,0.55", settings => settings.BlueColourWidth),
+      new SettingsTester(new Range(0, 1), "lightintensity:0,1", settings => settings.LightIntensityWidth),
+      new SettingsTester(new Range(0.4f, 0.9f), "fan:0.4,0.9", settings => settings.FanWidth),
+      new SettingsTester(500, "bpm:120", settings => settings.PushInterval)
+    };
+
+
+    [Test]
+    // Invalid number of arguments
+    [TestCase("red:0.1,0.5,0.9")]
+    [TestCase("red:0.7")]
+    // Arguments out of range
+    [TestCase("red:0.5,2")]
+    [TestCase("red:-1,0.2")]
+    [TestCase("red:2,4")]
+    public void SpecifyingACustomRange_WithInvalidData_ThrowsException(string xiRange)
+    {
+      var lArgument = new List<string> { xiRange };
+      var lArgumentReader = new ArgumentReader(lArgument);
+      Assert.Throws<UsageException>(() => lArgumentReader.ParseArguments());
     }
 
     private Settings mTestSettings;
   }
+
+  public class SettingsTester
+  {
+    public SettingsTester(object xiExpectedValue, string xiArgument, GetSetting xiRangeSelector)
+    {
+      ExpectedValue = xiExpectedValue;
+      Argument = xiArgument;
+      RangeSelector = xiRangeSelector;
+    }
+
+    public object ExpectedValue;
+    public string Argument;
+    public GetSetting RangeSelector;
+  }
+
+  public delegate object GetSetting(Settings xiSettings);
 }
