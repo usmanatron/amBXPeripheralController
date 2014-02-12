2d068daf17b8f9648e0c06f0932178c29e3c44ba
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-12 20:02:02 +0000

    SceneAccessor tests

---
+++ aPC.Common.Tests/SceneAccessorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using aPC.Common;
+using aPC.Common.Entities;
+using NUnit.Framework;
+
+namespace aPC.Common.Tests
+{
+  [TestFixture]
+  class SceneAccessorTests
+  {
+    [TestFixtureSetUp]
+    public void Setup()
+    {
+      mSeceneAccessor = new SceneAccessor();
+    }
+
+    [Test]
+    [TestCase("error_flash")]
+    [TestCase("Error_Flash")]
+    [TestCase("eRrOr_FLaSh")]
+    public void ValidSceneName_ReturnsScene_RegardlessOfCase(string xiSceneName)
+    {
+      var lScene = mSeceneAccessor.GetScene(xiSceneName);
+      Assert.IsNotNull(lScene);
+      Assert.IsTrue(lScene is amBXScene);
+    }
+
+    [Test]
+    public void InvalidSceneName_ReturnsNull()
+    {
+      Assert.IsNull(mSeceneAccessor.GetScene("TotallyInvalidScene"));
+    }
+
+    private SceneAccessor mSeceneAccessor;
+  }
+}
