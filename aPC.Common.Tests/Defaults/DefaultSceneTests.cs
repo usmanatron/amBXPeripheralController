a50d78b4f89b2b819be1fcb8aa92f090ae5357c5
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-12 19:40:10 +0000

    DefaultScene tests

---
+++ aPC.Common.Tests/Defaults/DefaultSceneTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using aPC.Common.Defaults;
+using aPC.Common.Entities;
+using NUnit.Framework;
+
+namespace aPC.Common.Tests.Defaults
+{
+  [TestFixture]
+  class DefaultSceneTests
+  {
+    [Test]
+    public void DefaultScenesAreValid()
+    {
+      Assert.DoesNotThrow(() => BuildAllDefaultScenes());
+    }
+
+    private void BuildAllDefaultScenes()
+    {
+      amBXScene lScene;
+
+      foreach (var lDefaultScene in GetScenes())
+      {
+        lScene = lDefaultScene;
+      }
+    }
+
+    private IEnumerable<amBXScene> GetScenes()
+    {
+      var lDefaultScenes = new DefaultScenes();
+
+      return lDefaultScenes
+        .GetType()
+        .GetProperties()
+        .Where(property => property.GetCustomAttribute<SceneNameAttribute>() != null)
+        .Select(property => property.GetValue(lDefaultScenes) as amBXScene);
+    }
+  }
+}
