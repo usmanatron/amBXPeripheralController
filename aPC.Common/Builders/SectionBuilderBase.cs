2fed57e4532bb29cfda074a5326ca24670374692
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-15 16:43:41 +0000

    Move Section-shared builder methods to SectionBuilderExtensions - changed to extension methods

--- aPC.Common/Builders/SectionBuilderBase.cs
+++ aPC.Common/Builders/SectionBuilderBase.cs
@@ -7,29 +7,6 @@ namespace aPC.Common.Builders
 {
   public class SectionBuilderBase
   {
-    public FieldInfo GetComponentInfoInDirection(SectionBase xiSection, eDirection xiDirection)
-    {
-      return GetSectionFields(xiSection)
-        .SingleOrDefault(field => DirectionAttribute.MatchesDirection(field, xiDirection));
-    }
-
-    public FieldInfo GetPhysicalComponentInfoInDirection(SectionBase xiSection, eDirection xiDirection)
-    {
-      var lFieldInrightDirection = GetComponentInfoInDirection(xiSection, xiDirection);
-
-      return PhysicalComponentAttribute.IsPhysicalDirection(lFieldInrightDirection)
-        ? lFieldInrightDirection
-        :
-        null;
-    }
-
-    private IEnumerable<FieldInfo> GetSectionFields(SectionBase xiSection)
-    {
-      return xiSection
-        .GetType()
-        .GetFields();
-    }
-
     protected void SetFadeTime(SectionBase xiSection, int xiFadeTime)
     {
       xiSection.FadeTime = xiFadeTime;
---
+++ aPC.Common/Entities/SectionBaseExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace aPC.Common.Entities
+{
+  public static class SectionBaseExtensions
+  {
+    public static FieldInfo GetComponentInfoInDirection(this SectionBase xiSection, eDirection xiDirection)
+    {
+      return GetSectionFields(xiSection)
+        .SingleOrDefault(field => DirectionAttribute.MatchesDirection(field, xiDirection));
+    }
+
+    public static FieldInfo GetPhysicalComponentInfoInDirection(this SectionBase xiSection, eDirection xiDirection)
+    {
+      var lFieldInrightDirection = GetComponentInfoInDirection(xiSection, xiDirection);
+
+      return PhysicalComponentAttribute.IsPhysicalDirection(lFieldInrightDirection)
+        ? lFieldInrightDirection
+        :
+        null;
+    }
+
+    private static IEnumerable<FieldInfo> GetSectionFields(this SectionBase xiSection)
+    {
+      return xiSection
+        .GetType()
+        .GetFields();
+    }
+  }
+}
