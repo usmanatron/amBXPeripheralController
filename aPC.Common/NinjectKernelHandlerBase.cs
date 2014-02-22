4a3a21b36abfa023c44705aceb42ee251e293338
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-22 22:47:57 +0000

    Add a NinjectKernelHandler class in an attempt to make things cleaner

---
+++ aPC.Common/NinjectKernelHandlerBase.cs
@@ -0,0 +1,22 @@
+﻿using Ninject;
+
+namespace aPC.Common
+{
+  public abstract class NinjectKernelHandlerBase
+  {
+    public NinjectKernelHandlerBase()
+    {
+      mKernel = new StandardKernel();
+    }
+
+    //qqUMI consider if object is alright?
+    protected abstract void SetupBindings(params object[] xiParams);
+
+    public T Get<T>()
+    {
+      return mKernel.Get<T>();
+    }
+
+    protected StandardKernel mKernel;
+  }
+}
