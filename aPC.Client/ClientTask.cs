da6c2b7ae8c8ebeff6b3e9d662ffcbe73cd90fef
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-22 23:06:12 +0000

    Rework Client to use INotificationClient

--- aPC.Client/ClientTask.cs
+++ aPC.Client/ClientTask.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using aPC.Common.Communication;
+using aPC.Common;
 
 namespace aPC.Client
 {
@@ -10,6 +11,8 @@ namespace aPC.Client
   {
     public ClientTask(IEnumerable<string> xiArguments)
     {
+      mNotificationClient = new Communication.NotificationClient();
+
       try
       {
         mArgs = new ArgumentReader(xiArguments.ToList());
@@ -23,24 +26,17 @@ namespace aPC.Client
 
     public void Push()
     {
-      //This client only supports pushing to localhost
-      var lClient = new ChannelFactory<INotificationService>(
-        new BasicHttpBinding(),
-        CommunicationSettings.ServiceUrlTemplate
-        .Replace(CommunicationSettings.HostnameHolder, "localhost"));
-
-      var lOutput = mArgs.IsIntegratedScene 
-        ? lClient.CreateChannel().RunIntegratedScene(mArgs.SceneName) 
-        : lClient.CreateChannel().RunCustomScene(mArgs.SceneXml);
-
-      if (!string.IsNullOrEmpty(lOutput))
+      if (mArgs.IsIntegratedScene)
       {
-        Console.WriteLine("An error occurred when communicating to the server:");
-        Console.WriteLine(Environment.NewLine);
-        Console.WriteLine(lOutput);
+        mNotificationClient.PushIntegratedScene(mArgs.SceneName);
+      }
+      else
+      {
+        mNotificationClient.PushCustomScene(mArgs.SceneXml);
       }
     }
 
     private readonly ArgumentReader mArgs;
+    private INotificationClient mNotificationClient;
   }
 }
---
+++ aPC.Client/Communication/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using aPC.Common.Communication;
+using System.ServiceModel;
+using System.IO;
+using aPC.Common;
+
+namespace aPC.Client.Communication
+{
+  class NotificationClient : INotificationClient
+  {
+    public NotificationClient()
+    {
+      //This client only supports pushing to localhost
+      mClient = new ChannelFactory<INotificationService>(
+        new BasicHttpBinding(),
+        CommunicationSettings.ServiceUrlTemplate
+        .Replace(CommunicationSettings.HostnameHolder, "localhost"));
+    }
+
+    public void PushCustomScene(string xiScene)
+    {
+      mClient.CreateChannel().RunCustomScene(xiScene);
+    }
+
+    public void PushIntegratedScene(string xiScene)
+    {
+      mClient.CreateChannel().RunIntegratedScene(xiScene);
+    }
+
+    private ChannelFactory<INotificationService> mClient;
+  }
+}
