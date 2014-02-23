2f23b8638b94263fb733daa17045559544228f3e
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-23 09:25:26 +0000

    Setup empty Client NinjectKernelHandler

---
+++ aPC.Client/NinjectKernelHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using aPC.Common.Communication;
+
+namespace aPC.Client
+{
+  class NinjectKernelHandler : NinjectKernelHandlerBase
+  {
+    public NinjectKernelHandler() :base()
+    {
+
+    }
+
+    protected override void SetupBindings()
+    {
+    }
+  }
+}
