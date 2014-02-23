3b607b1a4bc7f58656cd9957e81508b0b04ae450
Usman Iqbal <usman.iqbal@softwire.com>
2014-02-23 21:06:34 +0000

    Add a test Notification Client

---
+++ aPC.Client.Disco.Tests/TestNotificationClient.cs
@@ -0,0 +1,36 @@
+﻿using aPC.Common.Communication;
+
+namespace aPC.Client.Disco.Tests
+{
+  class TestNotificationClient : INotificationClient
+  {
+    public TestNotificationClient()
+    {
+      mIntegratedScenesPushed = 0;
+      mCustomScenesPushed = 0;
+    }
+
+    public override void PushCustomScene(string xiScene)
+    {
+      mCustomScenesPushed++;
+    }
+
+    public override void PushIntegratedScene(string xiScene)
+    {
+      mIntegratedScenesPushed++;
+    }
+
+    public int NumberOfCustomScenesPushed
+    {
+      get { return mCustomScenesPushed; }
+    }
+
+    public int NumberOfIntegratedScenesPushed
+    {
+      get { return mIntegratedScenesPushed; }
+    }
+
+    private int mIntegratedScenesPushed;
+    private int mCustomScenesPushed;
+  }
+}
