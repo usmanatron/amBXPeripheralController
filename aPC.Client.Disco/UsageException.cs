e0715ce20abb5a853ceec5502f3d4f806e1fdcc0
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-02 10:25:49 +0000

    Better Disco Client usage description

--- aPC.Client.Disco/UsageException.cs
+++ aPC.Client.Disco/UsageException.cs
@@ -19,12 +19,17 @@ namespace aPC.Client.Disco
 All arguments of the form A:B where A is the name and B is the value.
 The following are supported:
 
-BPM : Beats per minute
+BPM       : Beats per minute.  Expects an integer value.  Default is 150
+            Example - BPM:300
 
-red, blue, green, fan
+Red \     : The range of colour to use when building random scenes.  Expects 
+Blue \      two values between 0 and 1 (a min and max).   the default for 
+Green \     all colours is a range between 0 and 1 (i.e. the full colour
+Intensity   spectrum).
+            Example - red:0.5,1 means that all lights generated will have 
+            a red component of at least 0.5 and at most 1
 
-qqUMI
-
+Fan   : A fan range (deprecated - do not use!)
 ";
     }
   }
