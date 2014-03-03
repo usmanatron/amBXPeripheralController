2c9d743f0bd039d4a8bc9cfd2c53d08cba56f65f
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-03 21:50:50 +0000

    Add framework for testing SceneHandlerBase (and a couple of tests, one of which doesn't yet work)

---
+++ aPC.Common.Server.Tests/SceneHandlers/SceneHandlerBaseTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using aPC.Common.Entities;
+
+namespace aPC.Common.Server.Tests.SceneHandlers
+{
+  [TestFixture]
+  class SceneHandlerBaseTests
+  {
+    [TestFixtureSetUp]
+    public void FixtureSetup()
+    {
+      mInitialScene = new SceneAccessor().GetScene("default_redvsblue");
+    }
+
+    [Test]
+    public void NewSceneHandler_IsNotDormant()
+    {
+      var lHandler = new TestSceneHandler();
+      Assert.IsFalse(lHandler.IsDormant);
+    }
+
+    //[Test] //qqUMI - doesn't work yet.  Need to override equals
+    public void GetNextFrame_GetsExpectedData()
+    {
+      var lHandler = new TestSceneHandler();
+      
+      var lFrame = lHandler.NextFrame;
+      var lExpectedFrame = mInitialScene.Frames.First();
+
+      Assert.AreEqual(lExpectedFrame.Length, lFrame.Length);
+      Assert.AreEqual(lExpectedFrame.IsRepeated, lFrame.IsRepeated);
+      Assert.AreEqual(lExpectedFrame.Lights, lFrame.Lights);
+    }
+
+    /* Check GetNextFrame works
+     * Check Advance works (do advance then check second frame against expected
+     * 
+     * Check UpdateScene works - confirm Scene is newand check first frame
+     * Check events work
+    */
+
+
+    [Test]
+    public void UpdateScene_UpdatesScene()
+    {
+    }
+
+    private amBXScene mInitialScene;
+  }
+}
---
+++ aPC.Common.Server.Tests/SceneHandlers/TestSceneHandler.cs
@@ -0,0 +1,39 @@
+﻿using aPC.Common.Entities;
+using aPC.Common.Server.SceneHandlers;
+using aPC.Common.Server.Tests.Snapshots;
+using System;
+
+namespace aPC.Common.Server.Tests.SceneHandlers
+{
+  class TestSceneHandler : SceneHandlerBase<TestSnapshot>
+  {
+    public TestSceneHandler() : this (null)
+    {
+    }
+
+    public TestSceneHandler(Action xiAction) : base (xiAction)
+    {
+    }
+
+    public override TestSnapshot GetNextSnapshot(eDirection xiDirection)
+    {
+      return new TestSnapshot();
+    }
+
+    public Frame NextFrame
+    {
+      get
+      {
+        return GetNextFrame();
+      }
+    }
+
+    public amBXScene Scene
+    {
+      get
+      {
+        return CurrentScene;
+      }
+    }
+  }
+}
