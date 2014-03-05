62db06a20ffdcdce739488b2ff550516aa53dcd1
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-05 23:09:27 +0000

    FrameHandler tests

---
+++ aPC.Common.Server.Tests/SceneHandlers/TestFrameHandler.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using aPC.Common.Server.SceneHandlers;
+using aPC.Common.Entities;
+using aPC.Common.Defaults;
+using System;
+
+namespace aPC.Common.Server.Tests.SceneHandlers
+{
+  [TestFixture]
+  class TestFrameHandler
+  {
+    [TestFixtureSetUp]
+    public void FixtureSetup()
+    {
+      mScene = new DefaultScenes().Building;
+    }
+
+    [Test]
+    public void NextFrame_IsAsExpected()
+    {
+      var lHandler = new FrameHandler(mScene);
+      
+      var lSnapshot = lHandler.GetNextSnapshot(eDirection.North);
+      var lExpectedFrame = mScene.Frames[0];
+
+      Assert.AreEqual(lSnapshot.Frame.Length, lExpectedFrame.Length);
+      Assert.AreEqual(lSnapshot.Frame.IsRepeated, lExpectedFrame.IsRepeated);
+      Assert.AreEqual(lSnapshot.Frame.Lights, lExpectedFrame.Lights);
+    }
+
+    [Test]
+    [TestCaseSource("Directions")]
+    public void GetNextSnapshot_ReturnsExpectedFrame_RegardlessOfDirection(eDirection xiDirection)
+    {
+      var lHandler = new FrameHandler(mScene);
+
+      var lSnapshot = lHandler.GetNextSnapshot(xiDirection);
+      var lExpectedFrame = mScene.Frames[0];
+
+      Assert.AreEqual(lSnapshot.Frame.Length, lExpectedFrame.Length);
+      Assert.AreEqual(lSnapshot.Frame.IsRepeated, lExpectedFrame.IsRepeated);
+      Assert.AreEqual(lSnapshot.Frame.Lights, lExpectedFrame.Lights);
+    }
+
+
+    private readonly eDirection[] Directions = (eDirection[])Enum.GetValues(typeof(eDirection));
+    private amBXScene mScene;
+  }
+}
