f09c40ad92c24e4504c2c9d86a53fe0f5c519806
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-05 23:09:43 +0000

    EngineManagerTests - just a stub at the moment...

---
+++ aPC.Common.Server.Tests/EngineManagerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aPC.Common.Server.Tests
+{
+  class EngineManagerTests
+  {
+    /* Difficult to test this as it messes with the amBX library. That said, would be good if I could?
+     * At the least I should test the conversion stuff (or, even better, work out a better way of comparison)
+     */
+  }
+}
