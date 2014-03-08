8de510d0a3825ba97b19e39a694fcc8f8610582f
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-08 15:39:51 +0000

    Test NotificationService

---
+++ aPC.Common.Server.Tests/Communication/TestNotificationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using aPC.Common.Server.Communication;
+using aPC.Common.Entities;
+
+namespace aPC.Common.Server.Tests.Communication
+{
+  class TestNotificationService : NotificationServiceBase
+  {
+    public TestNotificationService()
+    {
+      PushedScenes = new List<amBXScene>();
+    }
+
+    protected override void UpdateScene(amBXScene xiScene)
+    {
+      PushedScenes.Add(xiScene);
+    }
+
+    public List<amBXScene> PushedScenes;
+  }
+}
