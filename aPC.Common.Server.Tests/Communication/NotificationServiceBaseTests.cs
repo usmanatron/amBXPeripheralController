4be25f63484ac339d2031bcb8fde52c26069967b
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-08 15:57:46 +0000

    Half complete NotificationService tests

---
+++ aPC.Common.Server.Tests/Communication/NotificationServiceBaseTests.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace aPC.Common.Server.Tests.Communication
+{
+  [TestFixture]
+  class NotificationServiceBaseTests
+  {
+    [Test]
+    public void PushedCustomScene_Updated()
+    {
+      var lService = new TestNotificationService();
+      lService.RunCustomScene(File.ReadAllText("ExampleScene.xml"));
+
+      Assert.AreEqual(1, lService.PushedScenes.Count);
+      //qq
+    }
+
+    [Test]
+    public void PushedValidIntegratedScene_Updated()
+    {
+      var lService = new TestNotificationService();
+      lService.RunIntegratedScene("Default_RedVsBlue");
+
+      Assert.AreEqual(1, lService.PushedScenes.Count);
+      //qq
+    }
+
+    [Test]
+    public void PushedInvalidIntegratedScene_DefaultstoErrorFlash()
+    {
+      var lService = new TestNotificationService();
+      lService.RunIntegratedScene("TotallyInvalidSceneName");
+
+      Assert.AreEqual(1, lService.PushedScenes.Count);
+    }
+  }
+}
