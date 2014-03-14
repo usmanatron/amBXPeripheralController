757081c93c5da7c45558aec22764264ff8528791
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-14 23:52:49 +0000

    Setup ConductorManager tests (and add first one)

---
+++ aPC.Server.Tests/ConductorManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using aPC.Common.Defaults;
+using aPC.Server;
+using NUnit.Framework;
+using System.Threading;
+
+namespace aPC.Server.Tests
+{
+  [TestFixture]
+  class ConductorManagerTests
+  {
+    [SetUp]
+    public void Setup()
+    {
+      mEngine = new TestEngineManager();
+      var lScene = new DefaultScenes().Building;
+      mConductorManager = new TestConductorManager(mEngine, lScene);
+    }
+
+    [Test]
+    public void EnablingSync_SetsFrameConductorRunning()
+    {
+      mConductorManager.EnableSync();
+      Thread.Sleep(100);
+
+      Assert.IsTrue(mConductorManager.FrameConductor.IsRunning);
+    }
+
+    private TestEngineManager mEngine;
+    private TestConductorManager mConductorManager;
+  }
+}
---
+++ aPC.Server.Tests/TestConductorManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using aPC.Server;
+using aPC.Server.Conductors;
+using aPC.Server.Engine;
+using aPC.Common.Entities;
+
+namespace aPC.Server.Tests
+{
+  class TestConductorManager : ConductorManager
+  {
+    public TestConductorManager(IEngine xiEngine, amBXScene xiScene) : base (xiEngine, xiScene, null)
+    {
+    }
+
+    public FrameConductor FrameConductor
+    {
+      get { return mFrameConductor; }
+    }
+
+    public List<LightConductor> LightConductors
+    {
+      get { return mLightConductors; }
+    }
+
+    public List<FanConductor> FanConductors
+    {
+      get { return mFanConductors; }
+    }
+
+    public List<RumbleConductor> RumbleConductors
+    {
+      get { return mRumbleConductors; }
+    }
+  }
+}
