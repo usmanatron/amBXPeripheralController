d3026094bb6480a416d55d4c9a1e4006bc7e1c97
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-15 15:46:31 +0000

    Another ConductorManager test

--- aPC.Server.Tests/ConductorManagerTests.cs
+++ aPC.Server.Tests/ConductorManagerTests.cs
@@ -7,6 +7,7 @@ using aPC.Common.Defaults;
 using aPC.Server;
 using NUnit.Framework;
 using System.Threading;
+using aPC.Common.Entities;
 
 namespace aPC.Server.Tests
 {
@@ -17,20 +18,44 @@ namespace aPC.Server.Tests
     public void Setup()
     {
       mEngine = new TestEngineManager();
-      var lScene = new DefaultScenes().Building;
+      var lDefaultScenes = new DefaultScenes();
+
+      var lScene = lDefaultScenes.Building;
+      mDifferentScene = lDefaultScenes.BuildBrokenAndBuilding;
       mConductorManager = new TestConductorManager(mEngine, lScene);
+
     }
 
     [Test]
     public void EnablingSync_SetsFrameConductorRunning()
     {
       mConductorManager.EnableSync();
-      Thread.Sleep(100);
+      Thread.Sleep(200);
 
       Assert.IsTrue(mConductorManager.FrameConductor.IsRunning);
     }
 
+    [Test]
+    public void UpdateSync_UpdatesSceneInFrameConductor()
+    {
+      mConductorManager.UpdateSync(mDifferentScene);
+    }
+
+
+
+    [Test]
+    public void EnablingDesync_SetsDesyncConductorsRunning()
+    {
+      mConductorManager.EnableDesync();
+      Thread.Sleep(200);
+
+      mConductorManager.LightConductors.ForEach(light => Assert.IsTrue(light.IsRunning));
+      mConductorManager.FanConductors.ForEach(fan => Assert.IsTrue(fan.IsRunning));
+      mConductorManager.RumbleConductors.ForEach(rumble => Assert.IsTrue(rumble.IsRunning));
+    }
+
     private TestEngineManager mEngine;
     private TestConductorManager mConductorManager;
+    private amBXScene mDifferentScene;
   }
 }
