3d5d7fd0cf4758b6af7ab79088f3449140ad33b1
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-16 19:48:36 +0000

    Add IConductor interface

---
+++ aPC.Server/Conductors/IConductor.cs
@@ -0,0 +1,21 @@
+﻿using aPC.Common;
+using aPC.Common.Entities;
+using aPC.Server.Snapshots;
+
+namespace aPC.Server.Conductors
+{
+  public interface IConductor
+  {
+    void Run();
+    void RunOnce();
+
+    void UpdateScene(amBXScene xiScene);
+
+    bool IsRunning { get; set; }
+    void Enable();
+    void Disable();
+
+    eDirection Direction { get; }
+    eComponentType ComponentType { get; }
+  }
+}
