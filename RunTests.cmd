ec124ca8f127b240c704a87a638689aaefad9c56
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-23 16:12:29 +0000

    Add Morse.Tests to the various scripts etc.

--- RunTests.cmd
+++ RunTests.cmd
@@ -3,3 +3,4 @@
 .\packages\NUnit.Runners.2.6.3\tools\nunit-console-x86.exe /framework:net-4.0 .\aPC.Server.Tests\bin\Debug\aPC.Server.Tests.dll /noshadow /result:shippable\testresults\aPC.Server.xml
 .\packages\NUnit.Runners.2.6.3\tools\nunit-console-x86.exe /framework:net-4.0 .\aPC.Client.Tests\bin\Debug\aPC.Client.Tests.dll /noshadow /result:shippable\testresults\aPC.Client.xml
 .\packages\NUnit.Runners.2.6.3\tools\nunit-console-x86.exe /framework:net-4.0 .\aPC.Client.Disco.Tests\bin\Debug\aPC.Client.Disco.Tests.dll /noshadow /result:shippable\testresults\aPC.Client.Disco.xml
+.\packages\NUnit.Runners.2.6.3\tools\nunit-console-x86.exe /framework:net-4.0 .\aPC.Client.Morse.Tests\bin\Debug\aPC.Client.Morse.Tests.dll /noshadow /result:shippable\testresults\aPC.Client.Morse.xml
