f387c8a42ac7f3dce8b6a0b0875de0cae2f62755
Usman Iqbal <usman.iqbal@softwire.com>
2014-03-25 22:01:25 +0000

    WordTranslator!

---
+++ aPC.Client.Morse.Tests/Translators/WordTranslatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace aPC.Client.Morse.Tests.Translators
+{
+  [TestFixture]
+  class WordTranslatorTests
+  {
+    [Test]
+    public void Test()
+    {
+    }
+
+    // Test a Letter has just the letter, no word separators.
+    // Test some simple words.  Have expected letter makeups and have character separators.
+  }
+}
