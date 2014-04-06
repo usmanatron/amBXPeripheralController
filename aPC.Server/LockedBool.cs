73f88a25641fea5f513663d516eea59ee45d449c
Usman Iqbal <usman.iqbal@softwire.com>
2014-04-06 22:03:54 +0100

    Add LockedBool

---
+++ aPC.Server/LockedBool.cs
@@ -0,0 +1,29 @@
+﻿namespace aPC.Server
+{
+  /// <summary>
+  /// A bool with locking to ensure it's thread-safe.
+  /// </summary>
+  public class LockedBool
+  {
+    public bool Value
+    {
+      get
+      {
+        lock (mValueLocker)
+        {
+          return mValue;
+        }
+      }
+      set
+      {
+        lock (mValueLocker)
+        {
+          mValue = value;
+        }
+      }
+    }
+
+    private bool mValue;
+    private readonly object mValueLocker = new object();
+  }
+}
