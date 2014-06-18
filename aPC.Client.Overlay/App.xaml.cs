b008cb010b2e08fabba7a71c4087d2b2c9b5de94
Usman Iqbal <usmanatron@hotmail.com>
2014-06-18 23:25:17 +0100

    Add console application support to the new overlay app (with the intention of retiring aPC.Client). Not yet entirely working

--- aPC.Client.Overlay/App.xaml.cs
+++ aPC.Client.Overlay/App.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
+using aPC.Common;
+using System.Threading;
 
 namespace aPC.Client.Overlay
 {
@@ -13,5 +17,37 @@ namespace aPC.Client.Overlay
   /// </summary>
   public partial class App : Application
   {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      var lArguments = GetArguments();
+
+      if (lArguments.Count > 0)
+      {
+        var parentId = ParentProcessUtilities.GetParentProcess(Process.GetCurrentProcess().Id).Id;
+        AttachConsole(parentId);
+        
+        Console.WriteLine("test");
+        Client.Main(lArguments.ToArray());
+        Thread.Sleep(1000);
+        Shutdown(0);
+      }
+
+      int i = 1;
+    }
+
+    // When retrieving command line arguments in this way, the first
+    // argument is always the name of the executable.
+    private List<string> GetArguments()
+    {
+      var lArgs = Environment.GetCommandLineArgs();
+      
+      return lArgs
+        .Skip(1)
+        .Take(lArgs.Count() - 1)
+        .ToList();
+    }
+
+    [DllImport("Kernel32.dll")]
+    public static extern bool AttachConsole(int processId);
   }
 }
