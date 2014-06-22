fa1cd4ad2c68f870ef414cd9b67581f792fda014
Usman Iqbal <usmanatron@hotmail.com>
2014-06-22 22:45:47 +0100

    Add base ClientApplication class, the idea being to use this in clients to hold all the vital parts

---
+++ aPC.Common.Client/ClientApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aPC.Common.Client
+{
+  class ClientApplication
+  {
+    public ISettings Settings;
+    public NinjectKernelHandlerBase Kernel;
+  }
+}
