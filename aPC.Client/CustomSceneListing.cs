1837956b8cb0cc6699d64b9f54e63e80849bd20a
Usman Iqbal <usmanatron@hotmail.com>
2014-06-23 23:12:23 +0100

    Add CustomSceneListing - will ultimately be used to handle the contents of the Custom Scene dropdown

---
+++ aPC.Client/CustomSceneListing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace aPC.Client
+{
+  class CustomSceneListing
+  {
+    public CustomSceneListing()
+    {
+      mBaseDirectory = Path.Combine(Environment.CurrentDirectory, "Profiles");
+      LoadSavedScenes();
+    }
+
+    private void LoadSavedScenes()
+    {
+      var lFiles = Directory.EnumerateFiles(mBaseDirectory);
+      foreach (var lFile in lFiles)
+      {
+        Scenes.Add(lFile, File.ReadAllText(Path.Combine(mBaseDirectory, lFile)));
+      }
+    }
+
+    // Includes path
+    public void ImportScene(string xifilename)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void DeleteScene(string xiFilename)
+    {
+      throw new NotImplementedException();
+      //Scenes.Remove(xiFilename);
+    }
+
+    private string mBaseDirectory;
+    public Dictionary<string, string> Scenes { get; private set; }
+  }
+}
