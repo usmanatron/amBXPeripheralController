5b12ef69fd4557e039a2695cf275a0041fcc4def
Usman Iqbal <usmanatron@hotmail.com>
2014-09-20 18:56:04 +0100

    Add FFT

--- aPC.Chromesthesia/ChromesthesiaTask.cs
+++ aPC.Chromesthesia/ChromesthesiaTask.cs
@@ -15,6 +15,11 @@ using aPC.Server.Engine;
 using NAudio.Wave;
 using System.Numerics;
 
+// The following link goes through understanding FFT and getting frequencies from it:
+// http://channel9.msdn.com/coding4fun/articles/AutotuneNET
+
+
+
 namespace aPC.Chromesthesia
 {
   class ChromesthesiaTask
@@ -24,6 +29,10 @@ namespace aPC.Chromesthesia
     private WasapiLoopbackCapture waveIn;
     private bool UseWCF = false;
     private int warmupTicker;
+    // There might be a sample aggregator in NAudio somewhere but I made a variation for my needs
+    private SampleAggregator sampleAggregator = new SampleAggregator(fftLength);
+    
+    private static int fftLength = 4096; // NAudio fft wants powers of two!
 
     public void Run()
     {
@@ -36,8 +45,11 @@ namespace aPC.Chromesthesia
         ServerUp();
       }
 
+      sampleAggregator.FftCalculated += new EventHandler<FftEventArgs>(FftCalculated);
+      sampleAggregator.PerformFFT = true;
+
       waveIn = new WasapiLoopbackCapture();
-      waveIn.DataAvailable += InputBufferToFileCallback;
+      waveIn.DataAvailable += OnDataAvailable;
       waveIn.StartRecording();
 
       Console.WriteLine(waveIn.WaveFormat.BitsPerSample);
@@ -76,7 +88,7 @@ namespace aPC.Chromesthesia
       return ret > 1 ? 1 : ret;
     }
 
-    public void InputBufferToFileCallback(object sender, WaveInEventArgs e)
+    public void OnDataAvailable(object sender, WaveInEventArgs e)
     {
       //qqUMI - The Buffer is probably Time / Amplitude.  Need FFT to turn into Freq / Amplitude (which'll give better colours?
       // http://social.msdn.microsoft.com/Forums/windowsdesktop/en-US/a41b5b12-b97b-4da8-ba31-739818db668d/wasapi-loopback-and-wmp-visualization-plugins?forum=windowspro-audiodevelopment
@@ -85,7 +97,24 @@ namespace aPC.Chromesthesia
       // http://naudio.codeplex.com/workitem/16401
       var transformedData = TransformData(e.Buffer, e.BytesRecorded);
 
-      var section = BuildLightSectionFromData(transformedData);
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    }
+
+    void FftCalculated(object sender, FftEventArgs e)
+    {
+      var section = BuildLightSectionFromData(e.Result.ToList());
       var frames = new FrameBuilder().AddFrame().WithLightSection(section).WithFrameLength(20).WithRepeated(true).Build();
 
       var scene = new amBXScene()
@@ -110,11 +139,13 @@ namespace aPC.Chromesthesia
         }
       }
 
-      if (transformedData.Item1.Count > 3600)
+      if (e.Result.Count() > 3600)
       {
-        var result = string.Format("{0} | {1} | {2}", transformedData.Item1[500], transformedData.Item1[2000], transformedData.Item1[3600]);
+        var result = string.Format("{0}--{1} | {2}--{3} | {4}--{5}", e.Result[500].X, e.Result[500].Y, e.Result[2000].X, e.Result[2000].Y, e.Result[3600].X, e.Result[3600].Y);
         Console.WriteLine(result);
       }
+
+      // Do something with e.result!
     }
 
     private Tuple<List<Single>, List<Single>> TransformData(byte[] buffer, int bytesRecorded)
@@ -130,12 +161,12 @@ namespace aPC.Chromesthesia
         var data = BitConverter.ToSingle(buffer, i * 4);
 
         if (isLeft)
-        {
-          leftData.Add(data);
+        { // qqUMI Both the same as I have to work out how I'm going to actually separate this!
+          sampleAggregator.Add(data);
         }
         else
         {
-          rightData.Add(data);
+          sampleAggregator.Add(data);
         }
         isLeft = !isLeft;
       }
@@ -143,21 +174,23 @@ namespace aPC.Chromesthesia
       return new Tuple<List<float>, List<float>>(leftData, rightData);
     }
 
-    private LightSection BuildLightSectionFromData(Tuple<List<Single>, List<Single>> transformedData)
+    private LightSection BuildLightSectionFromData(List<NAudio.Dsp.Complex> data)
     {
       // transformedData.Count is 35280/8 = 4410
-      var leftData  = transformedData.Item1;
-      var rightData = transformedData.Item2;
+      var leftData  = data;
+      var rightData = data;
 
+      var WData = MergeLists(leftData, rightData, 1, 0).ToList();
       var NWData = MergeLists(leftData, rightData, 0.75f, 0.25f).ToList();
-      var WData = MergeLists(leftData, rightData, 0.5f, 0.5f).ToList();
+      var NData = MergeLists(leftData, rightData, 0.5f, 0.5f).ToList();
       var NEData = MergeLists(leftData, rightData, 0.25f, 0.75f).ToList();
+      var EData = MergeLists(leftData, rightData, 0, 1).ToList();
 
-      var W = new Light { Intensity = 1, Blue = Normailise(AverageFromSubset(leftData, 400, 50)), Green = Normailise(AverageFromSubset(leftData, 2000, 50)), Red = Normailise(AverageFromSubset(leftData, 3600, 50)) };//5880, 10))};
+      var W = new Light { Intensity = 1, Blue = Normailise(AverageFromSubset(WData, 400, 50)), Green = Normailise(AverageFromSubset(WData, 2000, 50)), Red = Normailise(AverageFromSubset(WData, 3600, 50)) };//5880, 10))};
       var NW = new Light { Intensity = 1, Blue = Normailise(AverageFromSubset(NWData, 400, 50)), Green = Normailise(AverageFromSubset(NWData, 2000, 50)), Red = Normailise(AverageFromSubset(NWData, 3600, 50)) };//11760, 10)) };
-      var N = new Light { Intensity = 1, Blue = Normailise(AverageFromSubset(WData, 400, 50)), Green = Normailise(AverageFromSubset(WData, 2000, 50)), Red = Normailise(AverageFromSubset(WData, 3600, 50)) };//17640, 10)) };
+      var N = new Light { Intensity = 1, Blue = Normailise(AverageFromSubset(NData, 400, 50)), Green = Normailise(AverageFromSubset(NData, 2000, 50)), Red = Normailise(AverageFromSubset(NData, 3600, 50)) };//17640, 10)) };
       var NE = new Light { Intensity = 1, Blue = Normailise(AverageFromSubset(NEData, 400, 50)), Green = Normailise(AverageFromSubset(NEData, 2000, 50)), Red = Normailise(AverageFromSubset(NEData, 3600, 50)) };//23520, 10)) };
-      var E = new Light { Intensity = 1, Blue = Normailise(AverageFromSubset(rightData, 400, 50)), Green = Normailise(AverageFromSubset(rightData, 2000, 50)), Red = Normailise(AverageFromSubset(rightData, 3600, 50)) };//29399, 10)) };
+      var E = new Light { Intensity = 1, Blue = Normailise(AverageFromSubset(EData, 400, 50)), Green = Normailise(AverageFromSubset(EData, 2000, 50)), Red = Normailise(AverageFromSubset(EData, 3600, 50)) };//29399, 10)) };
 
       return new LightSectionBuilder().WithFadeTime(18)
           .WithLightInDirection(eDirection.West, W)
@@ -168,7 +201,7 @@ namespace aPC.Chromesthesia
           .Build();
     }
 
-    private IEnumerable<Single> MergeLists(IEnumerable<Single> firstList, IEnumerable<Single> secondList, float firstPercentage, float secondPercentage)
+    private IEnumerable<Single> MergeLists(IEnumerable<NAudio.Dsp.Complex> firstList, IEnumerable<NAudio.Dsp.Complex> secondList, float firstPercentage, float secondPercentage)
     {
       if (firstList.Count() != secondList.Count())
       {
@@ -179,7 +212,7 @@ namespace aPC.Chromesthesia
 
       for (int i = 1; i < firstList.Count(); i++)
       {
-        mergedList.Add((firstList.ElementAt(i) + secondList.ElementAt(i)) / 2);
+        mergedList.Add((firstList.ElementAt(i).X + secondList.ElementAt(i).X) / 2);
       }
 
       return mergedList;
