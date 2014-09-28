4772b0ce9001b42d5726367adb4b9bcf3edcb6ee
Usman Iqbal <usmanatron@hotmail.com>
2014-09-28 16:44:26 +0100

    Remove initial FFT method (now uses NAudio for FFT)

--- aPC.Chromesthesia/Pitch/FftPitchDetector.cs
+++ aPC.Chromesthesia/Pitch/FftPitchDetector.cs
@@ -7,37 +7,31 @@ namespace aPC.Chromesthesia.Pitch
 {
   /* qqUMI TODO:
    * Increase buffer to 8192 if possible (decrease bin size)
-   * Use NAudio to calculate FFT (instead of some random library)
    */
 
   // FFT based pitch detector. seems to work best with block sizes of 4096
   public class FftPitchDetector : IPitchDetector
   {
     private float sampleRate;
+    private Complex[] fftBufferComplex;
+    private Complex[] prevBufferComplex;
 
     public FftPitchDetector(float sampleRate)
     {
       this.sampleRate = sampleRate;
     }
 
-    private float[] fftBuffer;
-    private Complex[] fftBufferComplex;
-    private float[] prevBuffer;
-    private Complex[] prevBufferComplex;
-
     public float DetectPitch(float[] buffer, int inFrames)
     {
-      if (prevBuffer == null)
+      if (prevBufferComplex == null)
       {
-        prevBuffer = new float[inFrames];
         prevBufferComplex = new Complex[inFrames];// / 2];
       }
 
       // double frames since we are combining present and previous buffers
       int frames = inFrames * 2;
-      if (fftBuffer == null)
+      if (fftBufferComplex == null)
       {
-        fftBuffer = new float[frames * 2]; // times 2 because it is complex input
         fftBufferComplex = new Complex[frames];
       }
 
@@ -45,60 +39,43 @@ namespace aPC.Chromesthesia.Pitch
       {
         if (n < inFrames)
         {
-
-          fftBuffer[n * 2] = prevBuffer[n] * (float) FastFourierTransform.HammingWindow(n, frames);
-          fftBufferComplex[n].X = prevBufferComplex[n].X * (float)FastFourierTransform.HammingWindow(n, frames);
-          
-          fftBuffer[n * 2 + 1] = 0; // need to clear out as fft modifies buffer
+          fftBufferComplex[n].X = GetIntensity(prevBufferComplex[n]) * (float)FastFourierTransform.HammingWindow(n, frames);
           fftBufferComplex[n].Y = 0;
         }
         else
         {
-          fftBuffer[n * 2] = buffer[n - inFrames] * (float) FastFourierTransform.HammingWindow(n, frames);
           fftBufferComplex[n].X = buffer[n - inFrames] * (float) FastFourierTransform.HammingWindow(n, frames);
-          
-          fftBuffer[n * 2 + 1] = 0; // need to clear out as fft modifies buffer
           fftBufferComplex[n].Y = 0;
         }
       }
 
-      // assuming frames is a power of 2
-      SmbPitchShift.smbFft(fftBuffer, frames, -1);
-      FastFourierTransform.FFT(true, fftBufferComplex.Length /*11*/, fftBufferComplex);
+      var power = (int) Math.Log(frames, 2);
+      FastFourierTransform.FFT(true, power, fftBufferComplex);
 
       float binSize = sampleRate / frames;
       int minBin = (int)(60 / binSize);
       int maxBin = (int)(600 / binSize);
       float maxIntensity = 0f;
-      float maxIntensityC = 0f;
       int maxBinIndex = 0;
-      int maxBinIndexC = 0;
       for (int bin = minBin; bin <= maxBin; bin++)
       {
-        float real = fftBuffer[bin * 2];
-        float realC = fftBufferComplex[bin].X;
-
-        float imaginary = fftBuffer[bin * 2 + 1];
-        float imaginaryC = fftBufferComplex[bin].Y;
-
-        float intensity = real * real + imaginary * imaginary;
-        float intensityC = realC * realC * imaginaryC * imaginaryC;
+        float real = fftBufferComplex[bin].X;
+        float imaginary = fftBufferComplex[bin].Y;
+        float intensity = real * real * imaginary * imaginary;
 
         if (intensity > maxIntensity)
         {
           maxIntensity = intensity;
           maxBinIndex = bin;
         }
-        if (intensityC > maxIntensityC)
-        {
-          maxIntensityC = intensityC;
-          maxBinIndexC = bin;
-        }
-
       }
-      if (maxBinIndex != maxBinIndexC) maxBinIndexC++;
 
       return binSize * maxBinIndex;
+    }
+
+    private float GetIntensity(Complex value)
+    {
+      return (float)Math.Sqrt((double)(value.X * value.X * value.Y * value.Y));
     }
   }
 }
