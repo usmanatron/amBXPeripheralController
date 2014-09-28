6b715fd933a246ca05a8064e2f1abc301a0d8701
Usman Iqbal <usmanatron@hotmail.com>
2014-09-28 14:57:21 +0100

    Implement basic FFT and break up pitch detection classes

---
+++ aPC.Chromesthesia/Pitch/IPitchDetector.cs
@@ -0,0 +1,7 @@
+﻿namespace aPC.Chromesthesia.Pitch
+{
+  public interface IPitchDetector
+  {
+    float DetectPitch(float[] buffer, int frames);
+  }
+}
---
+++ aPC.Chromesthesia/Pitch/PitchDetector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aPC.Chromesthesia.Pitch
+{
+  class PitchDetector
+  {
+    private AutoCorrelator autoCorrelator;
+    private FftPitchDetector fftPitchDetector;
+    private bool UseAutoCorrelator;
+
+    private int maxHold;
+    private int release;
+    private float previousPitch;
+
+    public PitchDetector()
+    {
+      int sampleRate = 44100;
+      autoCorrelator = new AutoCorrelator(sampleRate);
+      fftPitchDetector = new FftPitchDetector(sampleRate);
+
+      UseAutoCorrelator = false;
+      maxHold = 1;
+    }
+
+    public float DetectPitch(float[] buffer, int frames)
+    {
+      float pitch;
+
+      if (UseAutoCorrelator)
+      {
+        pitch = autoCorrelator.DetectPitch(buffer, frames);
+      }
+      else
+      {
+        pitch = fftPitchDetector.DetectPitch(buffer, frames);
+      }
+
+      pitch = StabilisePitch(pitch);
+      return pitch;
+    }
+
+    private float StabilisePitch(float pitch)
+    {
+      // an attempt to make it less "warbly" by holding onto the pitch 
+      // for at least one more buffer
+      if (pitch == 0f && release < maxHold)
+      {
+        pitch = previousPitch;
+        release++;
+      }
+      else
+      {
+        this.previousPitch = pitch;
+        release = 0;
+      }
+
+      return pitch;
+    }
+  }
+}
