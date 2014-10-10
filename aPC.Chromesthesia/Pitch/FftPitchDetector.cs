b50e05ed09fa6f0c98d7e7cba28b895f82c8bf64
Usman Iqbal <usmanatron@hotmail.com>
2014-10-10 23:24:42 +0100

    Actually save the previous buffer...

--- aPC.Chromesthesia/Pitch/FftPitchDetector.cs
+++ aPC.Chromesthesia/Pitch/FftPitchDetector.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections.Generic;
 using NAudio.Dsp;
 using System;
+using System.Linq;
 
 namespace aPC.Chromesthesia.Pitch
 {
   /* TODO: Increase buffer to 8192 if possible (decrease bin size) */
-
+  // 1024 frames are input
   // FFT based pitch detector. seems to work best with block sizes of 4096
   public class FftPitchDetector : IPitchDetector
   {
     private readonly float sampleRate;
     private Complex[] fftBuffer;
-    private Complex[] prevBuffer;
+    private float[] prevBuffer;
 
     // Settings
     private const int lowerDetectionFrequency = 60;
@@ -26,7 +27,7 @@ namespace aPC.Chromesthesia.Pitch
     {
       if (prevBuffer == null)
       {
-        prevBuffer = new Complex[inFrames];// / 2];
+        prevBuffer = new float[buffer.Count()];
       }
 
       // double frames since we are combining present and previous buffers
@@ -40,7 +41,7 @@ namespace aPC.Chromesthesia.Pitch
       {
         if (n < inFrames)
         {
-          fftBuffer[n].X = GetIntensity(prevBuffer[n]) * (float)FastFourierTransform.HammingWindow(n, frames);
+          fftBuffer[n].X = prevBuffer[n] * (float)FastFourierTransform.HammingWindow(n, frames);
           fftBuffer[n].Y = 0;
         }
         else
@@ -53,7 +54,9 @@ namespace aPC.Chromesthesia.Pitch
       var power = (int) Math.Log(frames, 2);
       FastFourierTransform.FFT(true, power, fftBuffer);
 
-      return BuildResults(fftBuffer, frames);
+      var result = BuildResults(fftBuffer, frames);
+      UpdatePreviousBuffer(buffer);
+      return result;
     }
 
     private PitchResult BuildResults(Complex[] fftBuffer, int frames)
@@ -75,6 +78,11 @@ namespace aPC.Chromesthesia.Pitch
     private float GetIntensity(Complex value)
     {
       return (float)Math.Sqrt((double)((value.X * value.X) + (value.Y * value.Y)));
+    }
+
+    private void UpdatePreviousBuffer(float[] fftBuffer)
+    {
+      prevBuffer = fftBuffer;
     }
   }
 }
