c7f5e2055380774ebf15ac224008d12acbaceb8c
Usman Iqbal <usmanatron@hotmail.com>
2014-10-19 11:00:06 +0100

    Remove pitch stabilisation from PeakPitch (only used for debugging information!).  Add totalAmplitude info

--- aPC.Chromesthesia/Pitch/PitchResult.cs
+++ aPC.Chromesthesia/Pitch/PitchResult.cs
@@ -7,56 +7,29 @@ namespace aPC.Chromesthesia.Pitch
   public class PitchResult
   {
     public List<Pitch> Pitches { get; private set; }
+    public float TotalAmplitude { get; private set; }
+    public Pitch PeakPitch { get; private set; }
 
-    private Pitch previousPitch;
-    private readonly int maxHold;
-    private int release;
-    
     public PitchResult(List<Pitch> pitches)
     {
       Pitches = pitches;
-      maxHold = 2;
+      TotalAmplitude = Pitches.Sum(pitch => pitch.amplitude);
+      PeakPitch = GetPeakPitch();
     }
 
-    public Pitch PeakPitch
+    private Pitch GetPeakPitch()
     {
-      get
+      // If they're all the same amplitude, we assume they're all empty => do nothing
+      if (Pitches.Max(pitch => pitch.amplitude) == Pitches.Min(pitch => pitch.amplitude))
       {
-        // If they're all the same amplitude, we assume they're all empty => do nothing
-        if (Pitches.Max(pitch => pitch.amplitude) == Pitches.Min(pitch => pitch.amplitude))
-        {
-          return new Pitch(0,0,0);
-        }
-
-        var currentPeakPitch = Pitches.Aggregate(
-          (pitch1, pitch2) => pitch1.amplitude > pitch2.amplitude 
-            ? pitch1 
-            : pitch2);
-
-
-        return StabilisePitch(currentPeakPitch);
-      }
-    }
-
-    // an attempt to make it less "warbly" by holding onto the pitch 
-    // for at least one more buffer
-    private Pitch StabilisePitch(Pitch currentPeakPitch)
-    {
-      if (previousPitch == null)
-      {
-        previousPitch = currentPeakPitch;
+        return new Pitch(0, 0, 0);
       }
 
-      if (Math.Abs(currentPeakPitch.fftBinIndex - previousPitch.fftBinIndex) == 1 && release < maxHold)
-      {
-        currentPeakPitch = previousPitch;
-        release++;
-      }
-      else
-      {
-        this.previousPitch = currentPeakPitch;
-        release = 0;
-      }
+      var currentPeakPitch = Pitches.Aggregate(
+        (pitch1, pitch2) => pitch1.amplitude > pitch2.amplitude
+          ? pitch1
+          : pitch2);
+
 
       return currentPeakPitch;
     }
