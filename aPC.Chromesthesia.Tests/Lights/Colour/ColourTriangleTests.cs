823d02ec7a96925cad6ec2211436d461d47322a4
Usman Iqbal <usmanatron@hotmail.com>
2015-01-09 21:57:15 +0000

    Rewrite ColourTriangle so it makes more sense, with the aid of tests

--- aPC.Chromesthesia.Tests/Lights/Colour/ColourTriangleTests.cs
+++ aPC.Chromesthesia.Tests/Lights/Colour/ColourTriangleTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using aPC.Chromesthesia.Lights.Colour;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,63 @@ using System.Threading.Tasks;
 
 namespace aPC.Chromesthesia.Tests.Lights.Colour
 {
-  class ColourTriangleTests
+  [TestFixture]
+  internal class ColourTriangleTests
   {
+    private const int lowerFrequency = 0;
+    private const int upperFrequency = 1000;
+
+    private int midpointFrequency
+    {
+      get { return (upperFrequency - lowerFrequency) / 2; }
+    }
+
+    private ColourTriangle triangle;
+
+    [SetUp]
+    public void Setup()
+    {
+      triangle = new ColourTriangle(new Tuple<int, int>(lowerFrequency, upperFrequency));
+    }
+
+    [Test]
+    public void MidpointOfTriangle_GivesMaximalValue()
+    {
+      var intensity = triangle.GetValue(midpointFrequency);
+
+      Assert.AreEqual(0.5, intensity);
+    }
+
+    [Test]
+    [TestCase(250)]
+    [TestCase(750)]
+    public void HalfwayBetweenMidpointAndEdge_GivesHalfOfMaximumValue(int frequency)
+    {
+      var intensity = triangle.GetValue(frequency);
+
+      Assert.AreEqual(0.25, intensity);
+    }
+
+    [Test]
+    [TestCase(lowerFrequency)]
+    [TestCase(upperFrequency)]
+    public void EdgesOfTriangle_ReturnZero(int frequency)
+    {
+      var intensity = triangle.GetValue(frequency);
+
+      Assert.AreEqual(0, intensity);
+    }
+
+    [Test]
+    [TestCase(-200)]
+    [TestCase(-1)]
+    [TestCase(-1001)]
+    [TestCase(12500)]
+    public void FrequencyOutsideOfTriangle_ReturnZero(int frequency)
+    {
+      var intensity = triangle.GetValue(frequency);
+
+      Assert.AreEqual(0, intensity);
+    }
   }
 }
--- aPC.Chromesthesia/Lights/Colour/ColourTriangle.cs
+++ aPC.Chromesthesia/Lights/Colour/ColourTriangle.cs
@@ -7,7 +7,7 @@ namespace aPC.Chromesthesia.Lights.Colour
   /// For a given start and end index, values between 0 and 1 are calculated so that,
   /// if graphed, you get a triangle, (with midPoint being the highest point).
   /// </summary>
-  internal class ColourTriangle : MidpointRadiusColourBuilder
+  public class ColourTriangle : MidpointRadiusColourBuilder
   {
     public ColourTriangle(Tuple<int, int> frequencyRange)
       : base(frequencyRange)
@@ -16,14 +16,17 @@ namespace aPC.Chromesthesia.Lights.Colour
 
     public override float GetValue(float frequency)
     {
-      var value = Math.Abs(frequency - midPoint) / radius;
+      var scaledFrequency = (frequency - midPoint + radius) / radius;
 
-      if (IsNotInRange(value))
+      var zeroCentredValue = scaledFrequency - 1;
+
+      if (IsNotInRange(zeroCentredValue))
       {
         return 0f;
       }
 
-      return value;
+      // Divide by 2 to ensure that the values returned are not too large (white-out)
+      return (1 - Math.Abs(zeroCentredValue)) / 2;
     }
 
     /// <remarks>
@@ -31,8 +34,7 @@ namespace aPC.Chromesthesia.Lights.Colour
     /// </remarks>
     private bool IsNotInRange(float value)
     {
-      return value > 1f ||
-             value < 0f;
+      return Math.Abs(value) > 1f;
     }
   }
 }
