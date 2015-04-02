274906ceea76f7c1c8b435cfbd8fb71ce42613a7
Usman Iqbal <usmanatron@hotmail.com>
2015-04-02 23:10:07 +0100

    Add communication classes (update action is missing, otherwise complete)

---
+++ aPC.ServerV3/Communication/CommunicationManager.cs
@@ -0,0 +1,49 @@
+﻿using aPC.Common.Communication;
+using System;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace aPC.ServerV3.Communication
+{
+  public class CommunicationManager : IDisposable
+  {
+    private ServiceHost host;
+
+    public CommunicationManager(INotificationService notificationService)
+    {
+      SetupHost(notificationService);
+      host.Open();
+    }
+
+    private void SetupHost(INotificationService notificationService)
+    {
+      string baseAddress = CommunicationSettings.GetServiceUrl(Dns.GetHostName(), eApplicationType.amBXPeripheralController);
+
+      host = new ServiceHost(notificationService.GetType(), new Uri(baseAddress));
+
+      AddHostBehaviors();
+      AddEndpoint();
+    }
+
+    private void AddHostBehaviors()
+    {
+      host.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true });
+    }
+
+    private void AddEndpoint()
+    {
+      host.AddServiceEndpoint(typeof(INotificationService), new BasicHttpBinding(), "");
+    }
+
+    public void Close()
+    {
+      host.Close();
+    }
+
+    public void Dispose()
+    {
+      Close();
+    }
+  }
+}
---
+++ aPC.ServerV3/Communication/NotificationService.cs
@@ -0,0 +1,66 @@
+﻿using aPC.Common;
+using aPC.Common.Communication;
+using aPC.Common.Defaults;
+using aPC.Common.Entities;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace aPC.ServerV3.Communication
+{
+  public class NotificationService : INotificationService
+  {
+    private Action<amBXScene> action;
+
+    // TODO: Add the right action to do the update
+    public NotificationService()
+      : this((scene) => { })
+    {
+    }
+
+    // Used for tests
+    public NotificationService(Action<amBXScene> updateScene)
+    {
+      action = updateScene;
+    }
+
+    public void RunCustomScene(string sceneXml)
+    {
+      var scene = DeserialiseScene(sceneXml);
+      UpdateScene(scene);
+    }
+
+    public void RunIntegratedScene(string sceneName)
+    {
+      var accessor = new SceneAccessor(new DefaultScenes());
+      var scene = accessor.GetScene(sceneName) ??
+                   accessor.GetScene("Error_Flash");
+
+      UpdateScene(scene);
+    }
+
+    public string[] GetSupportedIntegratedScenes()
+    {
+      var lAccessor = new SceneAccessor(new DefaultScenes());
+
+      return lAccessor.GetAllScenes()
+        .Select(scene => scene.Key)
+        .ToArray();
+    }
+
+    private amBXScene DeserialiseScene(string sceneXml)
+    {
+      using (var reader = new StringReader(sceneXml))
+      {
+        var serialiser = new XmlSerializer(typeof(amBXScene));
+        return (amBXScene)serialiser.Deserialize(reader);
+      }
+    }
+
+    protected void UpdateScene(amBXScene scene)
+    {
+      action(scene);
+    }
+  }
+}
