ad75c1870b03dbc3c37acff6aa68ecdc78bca191
Usman Iqbal <usmanatron@hotmail.com>
2015-04-03 14:27:12 +0100

    AtypicalFirstRunTicker (similar to the previous one; some improvements have been made). With tests!

---
+++ aPC.ServerV3.Tests/AtypicalFirstRunInfiniteTickerTests.cs
@@ -0,0 +1,137 @@
+﻿using aPC.ServerV3;
+using NUnit.Framework;
+using System;
+
+namespace aPC.ServerV3.Tests
+{
+  [TestFixture]
+  public class AtypicalFirstRunInfiniteTickerTests
+  {
+    [Test]
+    [TestCase(-5, 3)]
+    [TestCase(0, 3)]
+    public void NonPositiveInitialCount_ThrowsException(int initial, int subsequent)
+    {
+      Assert.Throws<InvalidOperationException>(() => CreateTicker(initial, subsequent));
+    }
+
+    [Test]
+    [TestCase(7, -5)]
+    [TestCase(7, -1)]
+    public void NegativeSubsequentCount_ThrowsException(int initial, int subsequent)
+    {
+      Assert.Throws<InvalidOperationException>(() => CreateTicker(initial, subsequent));
+    }
+
+    [Test]
+    public void NewTicker_WithZeroSubsequentCount_DoesNotThrow()
+    {
+      Assert.DoesNotThrow(() => CreateTicker(5, 0));
+    }
+
+    [Test]
+    public void NewTicker_StartsAtZero()
+    {
+      var ticker = CreateDefaultTicker();
+      Assert.AreEqual(0, ticker.Index);
+    }
+
+    [Test]
+    public void NewTicker_HasFirstRunTrue()
+    {
+      var ticker = CreateDefaultTicker();
+      Assert.AreEqual(true, ticker.IsFirstRun);
+    }
+
+    [Test]
+    public void NewTicker_TickedOnce_IncreasesByOne()
+    {
+      var ticker = CreateDefaultTicker();
+      ticker.Advance();
+      Assert.AreEqual(1, ticker.Index);
+    }
+
+    [Test]
+    public void NewTicker_AdvancingThroughFirstRun_ReturnsToZero()
+    {
+      var ticker = CreateTicker(firstRunTickerSize);
+
+      for (int ticks = 0; ticks < firstRunTickerSize; ticks++)
+      {
+        ticker.Advance();
+      }
+
+      Assert.AreEqual(0, ticker.Index);
+    }
+
+    [Test]
+    public void NewTicker_AdvancingThroughFirstRun_ChangesFirstRunFalse()
+    {
+      var ticker = CreateTicker(firstRunTickerSize);
+
+      for (int ticks = 0; ticks < firstRunTickerSize; ticks++)
+      {
+        ticker.Advance();
+      }
+
+      Assert.AreEqual(false, ticker.IsFirstRun);
+    }
+
+    [Test]
+    public void NewTickerWithDifferentSubsequentSize_AdvancingThroughTwoFullRuns_ReturnsToZero()
+    {
+      var ticker = CreateTicker(firstRunTickerSize, subsequentTickerSize);
+
+      // The following should complete two full runs: One at the smaller initial size and one at the larger subsequent one.
+      for (int ticks = 0; ticks < firstRunTickerSize + subsequentTickerSize; ticks++)
+      {
+        ticker.Advance();
+      }
+
+      Assert.AreEqual(0, ticker.Index);
+    }
+
+    [Test]
+    public void ResettingTicker_ResetsIndexAndFirstRun()
+    {
+      var ticker = CreateTicker(2, 1);
+      ticker.Advance();
+
+      ticker.Reset(2, 1);
+
+      Assert.AreEqual(0, ticker.Index);
+      Assert.IsTrue(ticker.IsFirstRun);
+    }
+
+    [Test]
+    public void ResettingTicker_ChangesCounts()
+    {
+      var ticker = CreateTicker(2, 1);
+      ticker.Advance();
+      ticker.Advance();
+
+      Assert.IsFalse(ticker.IsFirstRun);
+
+      ticker.Reset(3, 1);
+      ticker.Advance();
+      ticker.Advance();
+
+      Assert.IsTrue(ticker.IsFirstRun);
+    }
+
+    private AtypicalFirstRunInfiniteTicker CreateTicker(int firstRun, int subsequentRun = 42)
+    {
+      return new AtypicalFirstRunInfiniteTicker(firstRun, subsequentRun);
+    }
+
+    private AtypicalFirstRunInfiniteTicker CreateDefaultTicker()
+    {
+      return CreateTicker(42);
+    }
+
+    // Note: the tests above assume these to be non equal
+    private const int firstRunTickerSize = 3;
+
+    private const int subsequentTickerSize = 7;
+  }
+}
---
+++ aPC.ServerV3/AtypicalFirstRunInfiniteTicker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace aPC.ServerV3
+{
+  /// <summary>
+  /// an infinite ticker that deals with an atypical first run (e.g. the first run has more steps,
+  /// which are dropped in subsequent runs)
+  /// </summary>
+  public class AtypicalFirstRunInfiniteTicker
+  {
+    public int Index { get; private set; }
+
+    public bool IsFirstRun { get; private set; }
+
+    private int initialCount;
+    private int subsequentCount;
+
+    public AtypicalFirstRunInfiniteTicker(int initialCount, int subsequentCount)
+    {
+      ResetTicker(initialCount, subsequentCount);
+    }
+
+    public void Reset(int initialCount, int subsequentCount)
+    {
+      ResetTicker(initialCount, subsequentCount);
+    }
+
+    private void ResetTicker(int initialCount, int subsequentCount)
+    {
+      // It's fine to have a subsequentCount of zero (this denotes a single run).
+      if (initialCount <= 0 || subsequentCount < 0)
+      {
+        var error = string.Format("Attempted to create a ticker with non-positive inputs: {0}, {1}",
+          initialCount,
+          subsequentCount);
+        throw new InvalidOperationException(error);
+      }
+
+      this.initialCount = initialCount;
+      this.subsequentCount = subsequentCount;
+
+      Index = 0;
+      IsFirstRun = true;
+    }
+
+    public void Advance()
+    {
+      Index++;
+      var count = IsFirstRun ? initialCount : subsequentCount;
+
+      if (Index == count)
+      {
+        IsFirstRun = false;
+        Index = 0;
+      }
+    }
+  }
+}
