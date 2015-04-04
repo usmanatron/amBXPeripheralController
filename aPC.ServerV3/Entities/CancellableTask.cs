28127bbdadca13dee7b93e022f01c72677c922d1
Usman Iqbal <usmanatron@hotmail.com>
2015-04-04 23:23:06 +0100

    Add some entities which'll be used in further classes...

---
+++ aPC.ServerV3/Entities/CancellableTask.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace aPC.ServerV3.Entities
+{
+  internal class CancellableTask
+  {
+    public Task Task { get; private set; }
+
+    public CancellationTokenSource CancellationToken { get; private set; }
+
+    public CancellableTask(Task task, CancellationTokenSource cancellationToken)
+    {
+      this.Task = task;
+      this.CancellationToken = cancellationToken;
+    }
+  }
+}
---
+++ aPC.ServerV3/Entities/RunningDirectionalComponent.cs
@@ -0,0 +1,36 @@
+﻿using aPC.Common;
+using aPC.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aPC.ServerV3.Entities
+{
+  /// <summary>
+  ///   Encapsulates a running DirectionalComponent
+  /// </summary>
+  /// <remarks>
+  ///   This also encapsulates a Frame when running in Sync mode.  This is
+  ///   arguably an abuse of this class
+  /// </remarks>
+  internal class RunningDirectionalComponent
+  {
+    public amBXScene Scene { get; private set; }
+
+    public eDirection Direction { get; private set; }
+
+    public eComponentType? ComponentType { get; private set; }
+
+    public AtypicalFirstRunInfiniteTicker Ticker { get; private set; }
+
+    public RunningDirectionalComponent(amBXScene scene, eDirection direction, eComponentType? componentType, AtypicalFirstRunInfiniteTicker ticker)
+    {
+      this.Scene = scene;
+      this.Direction = direction;
+      this.ComponentType = componentType;
+      this.Ticker = ticker;
+    }
+  }
+}
