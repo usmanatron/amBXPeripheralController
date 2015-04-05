12d4987c39ed52dce1b4efc5a6853090f7313159
Usman Iqbal <usmanatron@hotmail.com>
2015-04-05 22:12:32 +0100

    Rename CancellableTask -> DirectionalComponentAction.  Remove Task and add componentType, direction

---
+++ aPC.ServerV3/Entities/DirectionalComponentAction.cs
@@ -0,0 +1,22 @@
+﻿using aPC.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace aPC.ServerV3.Entities
+{
+  internal class DirectionalComponentAction
+  {
+    public CancellationTokenSource CancellationToken { get; private set; }
+
+    public eComponentType? ComponentType { get; private set; }
+
+    public eDirection Direction { get; private set; }
+
+    public DirectionalComponentAction(CancellationTokenSource cancellationToken, eComponentType? compoenntType, eDirection direction)
+    {
+      this.CancellationToken = cancellationToken;
+      this.ComponentType = compoenntType;
+      this.Direction = direction;
+    }
+  }
+}
